c9007f3d7ab3c9939b27e5c7e6621ba84f463d8b
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2022-12-27 21:22:41 +0100

    Configure S3 as a source

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@ require("dotenv").config({
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-s3`,
+      options: {
+        aws: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+          endpoint: process.env.AWS_ENDPOINT,
+          region: process.env.AWS_REGION,
+        },
+        buckets: [process.env.S3_BUCKET],
+      },
+    },
+  ],
 }
